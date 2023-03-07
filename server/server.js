a88cb02a8fe17af0629a607face306c08784c269
yagox <yago.ramiresx@gmail.com>
2023-03-07 15:53:09 -0300

    testes no server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDatabase from "./config/MongoDb.js";
-import ImportData from "./DataImport.js";
-import productRoute from "./Routes/ProductRoutes.js";
-import { errorHandler, notFound } from "./Middleware/Errors.js";
-import userRouter from "./Routes/UserRoutes.js";
-import orderRouter from "./Routes/orderRoutes.js";
+import express, { Router } from 'express';
+import dotenv from 'dotenv';
+import connectDatabase from './config/MongoDb.js';
+import ImportData from './DataImport.js';
+import productRoute from './Routes/ProductRoutes.js';
+import { errorHandler, notFound } from './Middleware/Errors.js';
+import userRouter from './Routes/UserRoutes.js';
+import orderRouter from './Routes/orderRoutes.js';
 import cors from 'cors';
 
 dotenv.config();
@@ -15,13 +15,22 @@ app.use(express.json());
 app.use(cors());
 
 // API
-app.use("/api/import", ImportData);
-app.use("/api/products", productRoute);
-app.use("/api/users", userRouter);
-app.use("/api/orders", orderRouter);
-app.get("/api/config/paypal", (req, res) => {
+app.use('/api/import', ImportData);
+app.use('/api/products', productRoute);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
+const route = Router();
+route.get('/', (req, res) => {
+  return res.json({
+    success: true,
+    message: 'Sucesso!',
+  });
+});
+app.use(route);
 
 // ERROR HANDLER
 app.use(notFound);
